7eb6ee8c0dde8cec253eccbe212c5577a046861e
Gabriel Morales <da.gabrielmora@gmail.com>
2021-03-28 22:04:04 -0500

    Now performs in real-time 60 FPS. The threshold is weak, so it's probably better to compensate for moving the mask as well.

--- LiveSegment/SegmentedRenderer.swift
+++ LiveSegment/SegmentedRenderer.swift
@@ -22,6 +22,11 @@ class SegmentedRenderer {
     private var buff: CVPixelBuffer?
     internal var bufferPool: CVPixelBufferPool?
     
+    private var transpositionHistory = [CGPoint]()
+    private var maxHistoryCount = 15
+    
+    private var priorMask: CIImage?
+    
     public var pixelBuffer: CVBuffer? {
         get {
             return buff
@@ -56,10 +61,31 @@ class SegmentedRenderer {
         
         do {
             
+            let translationalRequest = VNTranslationalImageRegistrationRequest(targetedCVPixelBuffer: imagebuff)
+            try requestHandler!.perform([translationalRequest], on: imagebuff)
+            
+            if let successfulRequest = translationalRequest.results {
+                
+                let transposition = successfulRequest[0] as! VNImageTranslationAlignmentObservation
+                let transform = transposition.alignmentTransform
+      
+                let tranformCoordinate = CGPoint(x: transform.tx, y: transform.ty)
+                
+                transpositionHistory.append(tranformCoordinate)
+            }
+
+            if sceneRedundancyStable() {
+                return priorMask
+            }
+            
+            if transpositionHistory.count >= maxHistoryCount {
+                transpositionHistory.removeAll()
+            }
+            
             let visionRequest = VNCoreMLRequest(model: SegmentedRenderer.visionModel!)
             visionRequest.imageCropAndScaleOption = .scaleFill
-            
             try requestHandler!.perform([visionRequest], on: imagebuff)
+            
             let observations = visionRequest.results as? [VNPixelBufferObservation]
             
             if observations == nil {
@@ -82,11 +108,13 @@ class SegmentedRenderer {
             let scaleTransform = CGAffineTransform(scaleX: xScale, y: yScale)
 
             var alphaMatte = maskImage
-                .applyingFilter("CIGammaAdjust", parameters: ["inputPower": 0.0007])
+                .applyingFilter("CIColorThresholdOtsu")
                 .applyingFilter("CIBoxBlur", parameters: ["inputRadius":3.5])
 
             alphaMatte = alphaMatte.transformed(by: scaleTransform)
 
+            priorMask = alphaMatte
+            
             return alphaMatte
             
         } catch {
@@ -100,6 +128,36 @@ class SegmentedRenderer {
         preconditionFailure("Abstract method must be overriden.")
     }
     
+    
+    private func sceneRedundancyStable() -> Bool {
+        
+        // from https://developer.apple.com/documentation/vision/training_a_create_ml_model_to_classify_flowers
+        // manhattan distabce between two points on a coordinate system.
+        // if the distance average passes a threshold, this qualifies true/false
+        
+        // MD: | x_1 + x_2 | - | y_1 + y_2 |
+        // current assumption: the points stem from the prior frame in the array.
+        
+        // enough frames have been collected to analyze distance change
+        if transpositionHistory.count >= maxHistoryCount {
+            var movementTotals: CGPoint = CGPoint.zero
+            for coordinate in transpositionHistory {
+                
+                movementTotals.x += coordinate.x
+                movementTotals.y += coordinate.y
+                
+            }
+    
+            let mDistance = abs(movementTotals.x) - abs(movementTotals.y)
+            
+            if mDistance < 20 {
+                return true
+            }
+            
+        }
+            
+        return false
+    }
     
     internal func overlayMask(withMask mask: CIImage, andBackgroundImage backImage: CIImage) -> CIImage? {
         
